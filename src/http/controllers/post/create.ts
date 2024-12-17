ade8015d1bd3fbd7094df8df805cce02b23f5bcd
Wrub <viniciuswrub01@gmail.com>
2024-12-17 07:36:09 -0300

    feat: post request

--- src/http/controllers/post/create.ts
+++ src/http/controllers/post/create.ts
@@ -3,21 +3,21 @@ import { CreatePostUseCase } from "@/use-cases/create-post";
 import { Request, Response } from "express";
 import { z } from "zod";
 
-export async function create(req: Request, res: Response) {
+export async function create(req: Request, res: Response): Promise<void> {
   const registerBodySchema = z.object({
-    id: z.number(),
+    id: z.coerce.number(),
     title: z.string(),
     subTitle: z.string(),
     content: z.string(),
     createdOn: z.coerce.date(),
-    userId: z.number(),
+    userId: z.coerce.number(),
     file: z.any(),
   });
 
-  const { id, title, subTitle, content, createdOn, userId, file } =
-    registerBodySchema.parse(req.body);
-
   try {
+    const { id, title, subTitle, content, createdOn, userId, file } =
+      registerBodySchema.parse(req.body);
+
     const postRepository = new PostRepository();
     const createPostUseCase = new CreatePostUseCase(postRepository);
 
@@ -30,9 +30,24 @@ export async function create(req: Request, res: Response) {
       userId,
       file,
     });
+
+    res.status(201).send();
   } catch (error) {
     console.error(error);
 
-    throw new Error("Internal Server Error");
+    // Erros de validação do Zod
+    if (error instanceof z.ZodError) {
+      res.status(400).json({
+        message:
+          "Validation failed, check the format of the request and try again",
+        errors: error.errors,
+      });
+    }
+
+    // Outros erros inesperados
+    res.status(500).json({
+      message: "Internal Server Error",
+      error: error instanceof Error ? error.message : String(error),
+    });
   }
 }
--- src/http/controllers/post/routes.ts
+++ src/http/controllers/post/routes.ts
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { create } from "./create";
 
-export const postRoutes = Router();
+const postRoutes = Router();
 
 postRoutes.get("/", (req, res) => {
   res.send("Lista de posts");
 });
 
-// FIXME: Corrigir, esta retornando erro no zod como undefined, no terminal
 postRoutes.post("/", create);
 
 postRoutes.get("/:id", (req, res) => {
   const { id } = req.params;
   res.send(`Detalhes do post ${id}`);
 });
+
+export { postRoutes };
