1466fdff84a514410325744d6a3801a2280f3573
Souza Rodrigo <Rodrigo.Souza@miba.com>
2025-01-06 19:58:54 -0300

    second commit

--- src/interfaces/controllers/UserController.ts
+++ src/interfaces/controllers/UserController.ts
@@ -6,15 +6,33 @@ import updateUser from '../../use-cases/users/UpdateUser';
 import deleteUser from '../../use-cases/users/DeleteUser';
 
 export default {
-  async registerUser(req: Request, res: Response) {
-    const newUser = await createUser(req.body);
-    res.status(201).json(newUser);
-  },
+    async registerUser(req: Request, res: Response) {
+        const newUser = await createUser(req.body);
+        res.status(201).json(newUser);
+    },
 
-  async loginUser(req: Request, res: Response) {
-    const { email, password } = req.body;
-    const { user, token } = await loginUser(email, password);
-    if (user && token) {
-      res.json({ user, token });
-    } else {
-      res.status
+    async loginUser(req: Request, res: Response) {
+        const { email, password } = req.body;
+        const { user, token } = await loginUser(email, password);
+        if (user && token) {
+            res.json({ user, token });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    },
+
+    async getUserById(req: Request, res: Response) {
+        const user = await getUserById(parseInt(req.params.id));
+        res.json(user);
+    },
+
+    async updateUser(req: Request, res: Response) {
+        const updatedUser = await updateUser(parseInt(req.params.id), req.body);
+        res.json(updatedUser);
+    },
+
+    async deleteUser(req: Request, res: Response) {
+        await deleteUser(parseInt(req.params.id));
+        res.status(204).end();
+    },
+};
